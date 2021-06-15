55ed7bc6ef7c3dc03eb005f0ccf8d188cc0faa43
starodubov <s.s.starodubov@yandex.ru>
2021-06-15 17:32:09 +0400

    add proof of work

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,12 +1,14 @@
 const SHA256 = require("crypto-js/sha256")
+const {DIFFICULTY} = require("../config")
 
 class Block {
 
-    constructor(timestamp, lastHash, hash, data) {
+    constructor(timestamp, lastHash, hash, data, nonce) {
         this.timestamp = timestamp
         this.lastHash = lastHash
         this.hash = hash
         this.data = data
+        this.nonce = nonce
     }
 
     toString() {
@@ -14,29 +16,33 @@ class Block {
         Timestamp: ${this.timestamp},
         Last Hash: ${this.lastHash.substring(0, 10)},
         Hash     : ${this.hash.substring(0, 10)},
+        Nonce    : ${this.nonce},
         Data     : ${this.data}`
     }
 
     static genesis() {
-        return new Block(
-            "Genesis Time", "----", "f1r57-h45h", []
-        )
+        return new Block("Genesis Time", "----", "f1r57-h45h", [], 0)
     }
 
     static mineBlock(lastBlock, data) {
-        const timestamp = Date.now()
         const lastHash = lastBlock.hash
-        const hash = Block.buildHash(timestamp, lastHash, data)
-        return new Block(timestamp, lastHash, hash, data)
+        let hash, timestamp
+        let nonce = 0
+        do {
+            nonce++
+            hash = Block.buildHash(timestamp, lastHash, data, nonce)
+        } while (hash.substring(0, DIFFICULTY) !== "0".repeat(DIFFICULTY))
+
+        return new Block(timestamp, lastHash, hash, data, nonce)
     }
 
-    static buildHash(timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString()
+    static buildHash(timestamp, lastHash, data, nonce) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString()
     }
 
     static buildHashFromBlock(block) {
-        const {timestamp, lastHash, data} = block
-        return Block.buildHash(timestamp,lastHash,data)
+        const {timestamp, lastHash, data, nonce} = block
+        return Block.buildHash(timestamp, lastHash, data, nonce)
     }
 }
 

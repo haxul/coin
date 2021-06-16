e057730bc92672999f34bfb7a58649282944c39f
starodubov <s.s.starodubov@yandex.ru>
2021-06-16 20:34:03 +0400

    add tp to p2pserver

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const Wallet = require("../wallet")
+const TransactionPool = require("../wallet/transaction-pool")
+const Blockchain = require("../blockchain")
+const P2PServer = require("./p2p-server")
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 const app = express()
 
-const Blockchain = require("../blockchain")
 const blockchain = new Blockchain()
+const wallet = new Wallet()
+const transactionPool = new TransactionPool()
+const p2pServer = new P2PServer(blockchain, transactionPool)
 
-const P2PServer = require("./p2p-server")
-const p2pServer = new P2PServer(blockchain)
 
 app.use(bodyParser.json())
 
@@ -22,6 +27,21 @@ app.post("/mine", (req, res) => {
 
     p2pServer.syncChains()
     res.redirect("/blocks")
+})
+
+app.get("/transactions", (req, res) => {
+    res.json(transactionPool.transactions)
+})
+
+app.post("/transact", (req, res) => {
+    const {recipient, amount} = req.body
+    const transaction = wallet.createTransaction(recipient, amount, transactionPool)
+    p2pServer.broadCoastTransaction(transaction)
+    res.redirect("/transactions")
+})
+
+app.get("/public-key", (req, res) => {
+    res.json({publicKey: wallet.publicKey})
 })
 
 app.listen(HTTP_PORT, () => console.log(`server starts on port ${HTTP_PORT}`))

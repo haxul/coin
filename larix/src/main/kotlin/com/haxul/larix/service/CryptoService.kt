14a7a86ac4796c959f87f15ba020c1c243377889
haxul <s.s.starodubov@yandex.ru>
2021-06-29 17:08:34 +0400

    add sha256

--- larix/src/main/kotlin/com/haxul/larix/service/CryptoService.kt
+++ larix/src/main/kotlin/com/haxul/larix/service/CryptoService.kt
@@ -8,15 +8,15 @@ class CryptoService {
 
     fun cryptoHash(vararg els: String): String {
         els.sort()
-        val originString = els.joinToString(separator = "")
-        val digest = MessageDigest.getInstance("SHA-256")
+        val originString: String = els.joinToString(separator = "")
+        val digest: MessageDigest = MessageDigest.getInstance("SHA-256")
         val encodedHash: ByteArray = digest.digest(originString.encodeToByteArray())
-        val hexString: StringBuilder = StringBuilder(2 * encodedHash.size)
+        val hexSb: StringBuilder = StringBuilder(2 * encodedHash.size)
         for (element in encodedHash) {
-            val hex = Integer.toHexString(0xff and element.toInt())
-            if (hex.length == 1) hexString.append('0')
-            hexString.append(hex)
+            val hex: String = Integer.toHexString(0xff and element.toInt())
+            if (hex.length == 1) hexSb.append('0')
+            hexSb.append(hex)
         }
-        return hexString.toString()
+        return hexSb.toString()
     }
 }
